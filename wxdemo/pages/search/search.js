e7b6ac175dbcdfa44b692c5c4f0a95502bc5000f
ketu <cao9710@outlook.com>
2021-03-13 19:54:15 +0800

    wxxcxdemo1.2.0
    增加了快递单号搜索

--- wxdemo/pages/search/search.js
+++ wxdemo/pages/search/search.js
@@ -1,66 +1,63 @@
 // pages/search/search.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    billno:'',
+    show:false,
+    expTextName:'',
+    tel:'',
+    msg: "",
+    updateStr:'',
+    logoUrl:'',
+    mailinfolist:[]
   },
+search:function(e) {
+  const that = this
+  this.setData({billno : e.detail.value})
+  let nu = this.data.billno
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  wx.request({
+    url: `https://route.showapi.com/64-19`, 
+    data: {
+      showapi_appid: '563956',
+      showapi_sign: '81424faff58a4dad8106793fef4b48a6',
+      com:'auto',
+      nu: nu
+    },
+    header: {
+      'content-type': 'application/json' 
+    },
+    success (res) {
+      if (res.data.showapi_res_code === -1 || res.data.showapi_res_body.flag === false) { 
+        that.setData({show:false})
+        // that.logoUrl=''
+        wx.showToast({
+          title: '莫得信息',
+          icon: 'error',
+          duration: 1000
+        })
+        return 
+      } //查询不到直接返回
+      else{
+      that.setData({show:true})
+      console.log(res)
+      that.setData({expTextName:res.data.showapi_res_body.expTextName})
+      that.setData({tel:res.data.showapi_res_body.tel})
+      that.setData({msg:res.data.showapi_res_body.msg})
+      that.setData({updateStr:res.data.showapi_res_body.updateStr})
+      that.setData({logoUrl:res.data.showapi_res_body.logo})
+      that.setData({mailinfolist : res.data.showapi_res_body.data})
+      }
+      
+    }
+  })
 
-  },
+}
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
 
-  },
+  // onPullDownRefresh: function () {
+  //   console.log('您搁那拉啥呢？')
+  // },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
